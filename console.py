0df30c4b82e942f947c825a10cb6db9b82f562c8
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-16 17:11:53 +0300

    added to cosole.py

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Console interface module"""
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -27,6 +28,31 @@ class HBNBCommand(cmd.Cmd):
     def help_EOF(self):
         print("EOF to exit the program")
 
+    def do_create(self, line):
+        if (line == ""):
+            print("** class name missing  **")
+        elif (line != "BaseModel"):
+            print("** class doesn't exist **")
+        else:
+            new_obj = BaseModel()
+            new_obj.save()
+            print(new_obj.id)
+
+    def do_show(self, line):
+        if (line == ""):
+            print("** class name missing  **")
+        else:
+            words = line.split()
+        cls = words[0]
+        obj_id = words[1]
+        print("class={} id= {}".format(cls, obj_id))
+        if (cls != "BaseModel"):
+            print("** class doesn't exist **")
+        elif (obj_id == ""):
+            print("** instance id missing  **")
+        print(cls.obj_id)
+        # elif (cls.obj_id is None):
+        #     print("** no instance found **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
