a8d368fd91ffe9bec22dcb751be2c1ee27d493b5
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-18 04:23:36 +0000

    console 0.1 updated

--- console.py
+++ console.py
@@ -31,7 +31,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         if (line == ""):
-            print("** class name missing  **")
+            print("** class name missing **")
         # !!!!!!!!!!!!!!!!! dosesnt search for classes
         elif (line != "BaseModel"):
             print("** class doesn't exist **")
@@ -48,7 +48,7 @@ class HBNBCommand(cmd.Cmd):
         cls = words[0]
         found_obj = None
         if (len(words) != 2):
-            print("** instance id missing  **")
+            print("** instance id missing **")
             return
         # now class name is 'cls' and object id is 'obj_id'
         obj_id = words[1]
@@ -74,6 +74,98 @@ class HBNBCommand(cmd.Cmd):
         else:
             obj = obj_dict[obj_key]
             print(obj)
+
+    def do_destroy(self, line):
+        # empty command
+        if (line == ""):
+            print("** class name missing **")
+            return
+        words = line.split()
+        cls = words[0]
+        found_obj = None
+        # empty id
+        if (len(words) != 2):
+            print("** instance id missing **")
+            return
+        # now class name is 'cls' and object id is 'obj_id'
+        obj_id = words[1]
+        # get dict of all objects
+        obj_dict = storage.all()
+        obj_key = None
+        classf = None
+        for key in obj_dict.keys():
+            cls_name = key.split(".")[0]
+            if (cls_name == cls):
+                # class found !!
+                classf = 1
+                if (key.split(".")[1] == obj_id):
+                    # object found !!
+                    obj_key = key
+                    break
+        if (classf is None):
+            print("** class doesn't exist **")
+            return
+        elif (obj_key is None):
+            print("** no instance found **")
+            return
+        else:
+            # delete object and update json
+            del obj_dict[obj_key]
+            storage.save()
+
+    def do_all(self, line):
+        objects = storage.all()
+        object_list = []
+        class_list = []
+        # get class list
+        for key in objects.keys():
+            class_list.append(key.split(".")[0])
+
+        for key, value in objects.items():
+            if (line == ""):
+                object_list.append(str(value))
+            else:
+                if (line.split()[0] == key.split(".")[0]):
+                    object_list.append(str(value))
+        if (len(object_list) == 0):
+            print("** class doesn't exist **")
+            return
+        else:
+            print(object_list)
+
+    def do_update(self, line):
+        # empty command
+        if (line == ""):
+            print("** class name missing **")
+            return
+        words = line.split()
+        cls = words[0]
+        objects = storage.all()
+        classes = []
+        object_to_update = None
+        for key in objects.keys():
+            classes.append(key.split(".")[0])
+        if cls not in classes:
+            print("** class doesn't exist **")
+            return
+        # check for empty id
+        if (len(words) < 2):
+            print("** instance id missing **")
+            return
+        cls += "." + words[1]
+        print(cls)
+        for key, value in objects.items():
+            if (cls == key):
+                object_to_update = value
+        if object_to_update is None:
+            print("** no instance found **")
+        if len(words) < 3:
+            print("** attribute name missing **")
+            return
+        if not object_to_update.words[2]:
+            print("** value missing **")
+            return
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
