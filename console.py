89eee42a26bad17b2038a89ee536ae6f887a8d39
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-18 23:08:26 +0000

    added task 10

--- console.py
+++ console.py
@@ -4,6 +4,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 
@@ -11,7 +16,7 @@ class HBNBCommand(cmd.Cmd):
     """cmd interface for the project."""
 
     prompt = '(hbnb) '
-    classes = ("BaseModel", "User")
+    classes = ("BaseModel", "User", "Place", "State", "City", "Amenity", "Review")
 
     def emptyline(self):
         pass
